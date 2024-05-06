6c3d994b3c8900e3e789f62ae1e131ad72c9ce0d
mtprovin <63170323+mtprovin@users.noreply.github.com>
2024-05-05 20:03:19 -0700

    Tweak capacity

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -63,7 +63,7 @@ def get_capacity_plan():
 
         gold = totals['gold'] + 100
 
-    gold_available = round(gold*.8)
+    gold_available = round(gold*.7)
 
     # buys potion capacity until percent in use is less than threshold
     # buys 1 ml capacity for every 2 potion capacity
